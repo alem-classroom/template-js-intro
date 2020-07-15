8ef85f6fd4b9bb9d5d448c79fc64ea05d0a374ed
Ali Tlekbai <ali.tlekbai@gmail.com>
2020-07-15 19:12:10 +0600

    add async

---
+++ async/async.js
@@ -0,0 +1,23 @@
+const delay = (N) => {
+	// your code here
+}
+
+const fetchData = (url) => {
+	// your code here
+}
+
+const all = (promises) => {
+	// your code here
+}
+
+const race = (promises) => {
+	// your code here
+}
+
+// 🚫 don't edit this export
+module.exports = {
+    delay,
+    fetchData,
+    all,
+    race
+}
